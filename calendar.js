4bf57030de2e034da02939a8a263e5673462df6b
Samuel Martinez <martinezxsamuel@gmail.com>
2024-08-15 14:20:44 -0700

    Calendar added

---
+++ calendar.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        selectable: true,
+        events: [], // You can add events here
+        select: function(info) {
+            var title = prompt('Enter Appointment Title:');
+            if (title) {
+                calendar.addEvent({
+                    title: title,
+                    start: info.startStr,
+                    end: info.endStr,
+                    allDay: info.allDay
+                });
+            }
+            calendar.unselect();
+        }
+    });
+
+    calendar.render();
+});
